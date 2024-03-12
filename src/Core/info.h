dc7b5cd8da41553a174dc2f08a064716a069e7a1
LGriebsch <LnsGriebsch@gmail.com>
2024-03-12 02:42:54 +0100

    VersionCheck

--- src/Core/info.h
+++ src/Core/info.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MOD_VERSION "v3.0600"
-#define MOD_VERSION_NUM	MOD_VERSION " BO BLEIS AI edition"
-#define MOD_WINDOW_TITLE "BBCF Improvement Mod BO BLEIS AI"
+#define MOD_VERSION "v1.0000"
+#define MOD_VERSION_NUM	MOD_VERSION " AI edition"
+#define MOD_WINDOW_TITLE "BBCF Improvement Mod AI"
 
 
 #define MOD_FORCE_DISABLE_UPDATE_CHECK 0
@@ -11,7 +11,7 @@
 #define MOD_LINK_DISCORD L"https://discord.gg/j2mCX9s"
 #define MOD_LINK_FORUM L"https://steamcommunity.com/app/586140/discussions/0/3195868146163014015/"
 
-#define MOD_LINK_GITHUB L"https://github.com/KDing0/BBCF-Improvement-Mod/tree/CbrLibreModMerge"
-#define MOD_LINK_API_GITHUB_RELEASE L"https://api.github.com/repos/libreofficecalc/BBCF-Improvement-Mod/releases"
+#define MOD_LINK_GITHUB L"https://github.com/KDing0/BBCF-Improvement-Mod/tree/master"
+#define MOD_LINK_API_GITHUB_RELEASE L"https://api.github.com/repos/KDing0/BBCF-Improvement-Mod/releases"
 
 
